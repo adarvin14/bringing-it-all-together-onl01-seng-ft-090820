e0d71c32a7a00367e7c0dca0d759a1e08ba2eb36
Alex Darvin <amdarvin14@gmail.com>
2020-10-11 18:17:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,11 +45,11 @@ class Dog
   end
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = breed[2]
-    self.new(name: name, breed: breed, id: id)
-  end
+        id = row[0]
+        name = row[1]
+        breed = row[2]
+        self.new(name: name, breed: breed, id: id)
+    end
   
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
